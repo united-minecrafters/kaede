a53a2c6b15749590d586d2fda30f81bd2f084212
Daniel Nash <dannash987@gmail.com>
2020-06-17 19:10:10 -0500

    Change to blacklist

--- cogs/filters.py
+++ cogs/filters.py
@@ -14,7 +14,13 @@ class Filter(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        if before.content != after.content:
+            await self.on_message(after)
+
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        print(1)
         if message.author.bot:
             return
         if not message.guild:
@@ -23,28 +29,31 @@ class Filter(commands.Cog):
         content = message.content.lower()
 
         for r in f["role_whitelist"]:
-            if r in [a.id for a in message.author.roles]:
-                return
+            if r in [a.id for a in message.author.roles[0:1]]:
+                pass
 
-        for r in f["word_watchlist"]:
-            if re.match(fr"[\b-]{r}[-\b]", content):
+        for r in f["word_blacklist"]:
+            print(r)
+            if re.match(rf"(?:^|[-\b]){r}([-\b$]|$)", content):
                 try:
                     await message.author.send(f"Hey, {message.author.mention}, your message was removed because of a "
                                               f"blacklisted word. If you feel this was a mistake, let staff know.")
                     await message.author.send(quote(message.content))
                 except discord.Forbidden:
-                    pass
+                    await message.channel.send(f"Hey, {message.author.mention}, your message was removed because of a "
+                                               f"blacklisted word. If you feel this was a mistake, let staff know.")
                 await message.delete()
                 return
 
-        for r in f["token_watchlist"]:
+        for r in f["token_blacklist"]:
             if re.match(fr"{r}", content):
                 try:
                     await message.author.send(f"Hey, {message.author.mention}, your message was removed because of a "
                                               f"blacklisted word. If you feel this was a mistake, let staff know.")
                     await message.author.send(quote(message.content))
                 except discord.Forbidden:
-                    pass
+                    await message.channel.send(f"Hey, {message.author.mention}, your message was removed because of a "
+                                               f"blacklisted word. If you feel this was a mistake, let staff know.")
                 await message.delete()
                 return
 
@@ -55,9 +64,11 @@ class Filter(commands.Cog):
                                               f"blacklisted domain. If you feel this was a mistake, let staff know.")
                     await message.author.send(quote(message.content))
                 except discord.Forbidden:
-                    pass
+                    await message.channel.send(f"Hey, {message.author.mention}, your message was removed because of a "
+                                               f"blacklisted domain. If you feel this was a mistake, let staff know.")
                 await message.delete()
                 return
+
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(Filter(bot))
