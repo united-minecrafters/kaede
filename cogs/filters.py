496bfb0605ecea304c9fecf466c7614af476f351
Daniel Nash <dannash987@gmail.com>
2020-06-17 19:08:41 -0500

    Add url filter and immune roles

--- cogs/filters.py
+++ cogs/filters.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 from libs.config import config
 from libs.utils import quote
 
+url_regex = re.compile(r"(https?://[^\s]+)")
+
 
 class Filter(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -20,8 +22,12 @@ class Filter(commands.Cog):
         f = config["filters"]
         content = message.content.lower()
 
+        for r in f["role_whitelist"]:
+            if r in [a.id for a in message.author.roles]:
+                return
+
         for r in f["word_watchlist"]:
-            if re.match(fr"[\b-{r}[\-\b]", content):
+            if re.match(fr"[\b-]{r}[-\b]", content):
                 try:
                     await message.author.send(f"Hey, {message.author.mention}, your message was removed because of a "
                                               f"blacklisted word. If you feel this was a mistake, let staff know.")
@@ -42,6 +48,16 @@ class Filter(commands.Cog):
                 await message.delete()
                 return
 
+        for url in f["domain_blacklist"]:
+            if url in content:
+                try:
+                    await message.author.send(f"Hey, {message.author.mention}, your message was removed because of a "
+                                              f"blacklisted domain. If you feel this was a mistake, let staff know.")
+                    await message.author.send(quote(message.content))
+                except discord.Forbidden:
+                    pass
+                await message.delete()
+                return
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(Filter(bot))
